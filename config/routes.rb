762391a0df6e9b3e775b4433792a6d0ab4682541
taltk9 <taltk9@gmail.com>
2017-08-04 16:25:53 -0300

    Add API versioning Created api_controller.rb to manage specific version controller options

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :webhooks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :webhooks
+      end
+    end
+  end
 end
