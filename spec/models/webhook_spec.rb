54cd3fc4f7213f66b0a9f1114fdf859dbac868a7
taltk9 <taltk9@gmail.com>
2017-08-04 15:40:26 -0300

    Add tests for Webhook Model

--- spec/models/webhook_spec.rb
+++ spec/models/webhook_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Webhook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:project_name) }
+  it { should validate_presence_of(:url) }
+
+  it { should allow_value('http://bar.com').for(:url) }
+  it { should_not allow_value('foo.bar').for(:url) }
 end
